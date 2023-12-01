ea69a67ca90ddfa2b3e6e47a041769c4aa66c276
Antoine Brossault <antoinebrossault@gmail.com>
2023-12-01 17:13:26 +0100

    clean

--- src/app/app.functions/getData.js
+++ src/app/app.functions/getData.js
@@ -4,15 +4,7 @@ exports.main = async (context = {}, callback) => {
 
     const order = await axios.get(`https://partner-app.antoinebrossault.com/api/devices/${context.parameters.deviceNumber}`);
 
-    
-
-    // console.log(context.parameters.deviceNumber)
-
-    // console.log([deviceInfo[context.parameters.deviceNumber]]);
-
     return callback(order.data)
-
-    //return callback([order.data])
 
 };
 
